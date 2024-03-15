5756743b9b0587eb448cfc9a079a59c2aa39a562
nmatos <nmatos@pursuit.org>
2024-03-15 18:00:30 -0400

    made cells for grid

---
+++ pixel-painter/main.js
@@ -0,0 +1,10 @@
+let layout = document.getElementById('canvas')
+
+
+for(let i = 0;  i < 80; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        layout.appendChild(cell)
+    }
+
+
