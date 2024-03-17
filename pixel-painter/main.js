abbeb09884d6a491e69c12680b0018c0ddfd57a4
nmatos <nmatos@pursuit.org>
2024-03-17 15:47:40 -0400

    able to click on color, change currColor, and apply it to the canvas

--- pixel-painter/main.js
+++ pixel-painter/main.js
@@ -1,10 +1,41 @@
 let layout = document.getElementById('canvas')
+let palette = document.getElementById('palette');
+let currColor = document.getElementById('current-color')
+let colors = palette.querySelectorAll('.color')
+
+colors.forEach(color => {
+    color.addEventListener('click', () => {
+    currColor.style['background'] = color.style['background']
+    })
+})
+
 
 
 for(let i = 0;  i < 80; i++) {
-        let cell = document.createElement('div');
+    let cell = document.createElement('div');
         cell.classList.add('cell');
-        layout.appendChild(cell)
+        cell.addEventListener('mouseover', (e) => {
+            if(e.buttons === 1) {
+            cell.style['background'] = currColor.style['background']
+            }
+        })
+        cell.addEventListener('click', (e) => {
+            cell.style['background'] = currColor.style['background']
+        })
+
+       layout.append(cell) 
     }
 
+   
+   
+    
+
+    
+   
+
+
+   
+    
+    
+    console.log(currColor)
 
